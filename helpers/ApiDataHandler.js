bedcc99ec2902e294698da3ead867a4287e9bb2c
Karen200088 <k.h.a.200088@gmail.com>
2023-06-15 17:47:13 +0400

    add api data and error handlers

---
+++ helpers/ApiDataHandler.js
@@ -0,0 +1,24 @@
+class ApiDataHandler {
+
+    status;
+    message;
+    data;
+
+    constructor(status, message, data) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
+
+    emptyData(message) {
+        return new ApiDataHandler(200, message, [])
+    }
+
+    successRequest(status, message, data = []) {
+        return new ApiDataHandler(status, message, data);
+    }
+
+
+}
+
+export default new ApiDataHandler();
---
+++ helpers/ApiErrorHandler.js
@@ -0,0 +1,24 @@
+class ApiErrorHandler {
+
+    status;
+    message;
+    error;
+
+    constructor(status, message, error) {
+        this.status = status;
+        this.message = message;
+        this.error = error;
+    }
+
+    unauthorizedError() {
+        return new ApiErrorHandler(401,"User not authorized", [])
+    }
+
+    badRequest(status, message, errors = []) {
+        return new ApiErrorHandler(status, message, errors);
+    }
+
+
+}
+
+export default new ApiErrorHandler();
